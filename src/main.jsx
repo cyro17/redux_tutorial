3862907ca154102d47b3157a6cc99826a144a7b4
Cyro <coolronitchs@gmail.com>
2024-03-10 13:44:27 +0530

    connected with redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import "./index.css";
+import store from "./store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./store.js";
+store.dispatch({ type: "account/deposit", payload: 250 });
+console.log(store.getState());
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
